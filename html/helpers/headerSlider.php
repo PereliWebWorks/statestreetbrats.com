986f4c5d09a411fe69cbbd58b3def89f056e21c6
Andrew Pereli <drewpereli@gmail.com>
2016-11-10 16:29:52 -0600

    updated image slider

--- html/helpers/headerSlider.php
+++ html/helpers/headerSlider.php
@@ -3,11 +3,22 @@
     <div class="slider-inner">
         <ul>
             <?php 
-            $numImages = 10;
+            $numImages = 7;
+            $captions = array(
+                "Welcome to State Street Brats",
+                "Caption 2",
+                "Caption 3",
+                "Caption 4",
+                "Caption 5",
+                "Caption 6",
+                "Caption 7",
+            ); 
+
             for ($i = 1 ; $i <= $numImages ; $i++) :
             ?>
                 <li>
                     <a class="ns-img" href="images/header_slider/<?= $i ?>.jpg"></a>
+                    <div class="caption"><?= $captions[$i - 1] ?></div>
                 </li>
             <?php endfor ?>
             <!--
