1ebb4d4dbffd68229feb319d37f4cb2ea22fab60
Andrew Pereli <drewpereli@gmail.com>
2016-11-10 22:03:08 -0600

    added tabs on home page. Added catering form helper

---
+++ html/helpers/cateringForm.php
@@ -0,0 +1,6 @@
+<form>
+	<div class="form-group">
+		<label for="catering-name">Name</label>
+		<input type="text" class="form-control" id="catering-name" name="name"/>
+	</div>
+</form>
--- html/index.php
+++ html/index.php
@@ -45,20 +45,21 @@
 	<span class="col-xs-10">
 		<div id="tabs">
 		  <ul>
-		    <li><a href="#fragment-1">One</a></li>
-		    <li><a href="#fragment-2">Two</a></li>
-		    <li><a href="#fragment-3">Three</a></li>
+		    <li><a href="#fragment-1">Bachelor</a></li>
+		    <li><a href="#fragment-2">Bachelorette</a></li>
+		    <li><a href="#fragment-3">Frat/Sorority</a></li>
 		  </ul>
 		  <div id="fragment-1">
-		    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+		    We can accommodate any number of rowdy bachelors up to 40 in our VIP room or for those really wicked benders, up to 200 on an exclusive no holds barred party on our entire second floor. Either way, you get bar service dedicated to your group and the options to request wait staff, bouncer services, and food menus at discounted rates. You can provide decorations or cakes and we can hold them for you or help put them up and prepare them. We can also provide snacks for $3 person up to full meal service at $5.50 per plate. Rates for the 40 person room begin at 200 dollars. Rates to book the entire upstairs begin at 400 dollars. Please submit the form for a speedy reply. We can do keg sales! please submit the message form for current rates.
 		  </div>
 		  <div id="fragment-2">
-		    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+		    If you are finally ready to make the big move, let the biggest party of your life take place on our second floor! Rent the rumpus room and seat up to 40 of your BFF's in style or rent our whole second floor for a wild party on our dance floor. We can provide a DJ or you can bring your own DJ, or even an iPod to connect directly to our massive stereo system. We can also provide snacks for $3 person up to full meal service at $5.50 per plate. Rates for the 40 person room begin at 200 dollars. Rates to book the entire upstairs begin at 400 dollars. We can do keg service or bottle service please submit form for current market rates and a speedy response!
 		  </div>
 		  <div id="fragment-3">
-		    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+		    We can do wristbanded parties for 18+ groups of up to 200 party people on our second floor! This includes bouncer services. We do dinner parties offering food options including simple snacks from $3 person up to full meal service at $5.50 per plate. Smaller party room rental rates for the 40 person room begin at 200 dollars. Rates to book the entire upstairs begin at 400 dollars. Please submit the form for a speedy reply. We can do keg sales! Please submit the message for form current rates.
 		  </div>
 		</div>
+		<!-- tabulate the tabs -->
 		<script>
 		$( function() {
 		$( "#tabs" ).tabs();
@@ -66,6 +67,12 @@
 		</script>
 	</span>
 	<span class="col-xs-1"></span>
+</span>
+
+
+<!-- Catering form -->
+<span class="row">
+	<?php require_once("helpers/cateringForm.php"); ?>
 </span>
 
 
