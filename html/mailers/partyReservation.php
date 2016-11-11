63a733eb28bfc84be09272fa6915ff9d09db417e
Andrew Pereli <drewpereli@gmail.com>
2016-11-11 13:49:00 -0600

    updated party reservation mail message

--- html/mailers/partyReservation.php
+++ html/mailers/partyReservation.php
@@ -5,10 +5,13 @@
 	$sSBMsg; //The message being sent to state street brats;
 	$sSBEmail = "drewpereli@gmail.com";
 	$sSBSubject = "New Party Reservation";
+
 	$customerMsg; //The message being sent to the customer
-	$customerMsg = $_POST["email"];
+	$customerEmail = $_POST["email"];
 	$customerSubject = "Thank you for requesting a party reservation with State Street Brats!";
 
+	$headers = "From: StateStreetBrats@104.236.118.215";
+	
 	$valueNames = array(
 		"name",
 		"email",
@@ -31,7 +34,7 @@
 		"Group Name or Theme",
 		"Privacy",
 		"Additional Comments"
-	)
+	);
 
 	//First sent the ssb email
 	$sSBMsg = "New request for a party reservation.\n";
@@ -44,8 +47,15 @@
 	}
 	$sSBMsg .= $info;
 
-	mail($sSBEmail, $sSBSubject, $sSBMsg);
-	echo $sSBMsg;
+	$customerMsg = "Your party reservation request has been sent.\n"
+		. "If any of the following information is incorrect, please email us at "
+		. "StateStreetBrats@yahoo.com and let us know!\n";
+	$customerMsg .= $info;
+	$customerMsg .= "Thank you!\n--State Street Brats";
+
+	mail($sSBEmail, $sSBSubject, $sSBMsg, $headers);
+	mail($customerEmail, $customerSubject, $customerMsg, $headers);
+
 ?>
 
 
